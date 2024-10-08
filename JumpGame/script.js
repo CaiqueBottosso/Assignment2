20c1729a5a2d483ac12ec0f8d8fa6fd3abd3de1c
Luís Almeida Henriques <luisrafa7@gmail.com>
2024-10-08 16:00:15 +0200

    alien game

--- JumpGame/script.js
+++ JumpGame/script.js
@@ -15,11 +15,47 @@ let floorBottom = parseInt(
 let floorHeight = parseInt(
   window.getComputedStyle(floor).getPropertyValue("height")
 );
+let allScreen = document.getElementById("game");
 let isJumping = false;
 let upTime;
 let downTime;
 let displayScore = document.getElementById("score");
 let score = 0;
+
+const arrayObstacles = [
+  "./images/p-air.png",
+  "./images/p-basket.png",
+  "./images/p-cotton.png",
+  "./images/p-earth.png",
+  "./images/p-lava.png",
+  "./images/p-meat.png",
+  "./images/p-ocean.png",
+  "./images/p-pink.png",
+  "./images/p-sun.png",
+  "./images/rock.png",
+  "./images/ufo.png",
+];
+
+const arrayCharacters = [
+  "./images/alien.png",
+  "./images/alien2.png",
+  "./images/astro1.png",
+  "./images/astro2.png",
+  "./images/astro3.png",
+];
+
+const arrayBackground = [
+  "./images/bg1.jpg",
+  "./images/bg2.jpg",
+  "./images/bg3.jpg",
+  "./images/bg4.jpg",
+  "./images/bg5.jpg",
+  "./images/bg6.jpg",
+  "./images/bg7.jpg",
+  "./images/bg8.jpg",
+  "./images/bg9.jpg",
+  "./images/bg10.png",
+];
 
 const jump = () => {
   if (isJumping) {
@@ -57,12 +93,13 @@ const generateObstacles = () => {
   let randomTimeout = Math.floor(Math.random() * 1000) + 1000;
   let obstacleRight = -30;
   let obstacleBottom = 100;
-  let obstacleWidth = 30;
+  let obstacleWidth = Math.floor(Math.random() * (50 - 10 + 1) + 10);
   let obstacleHeight = Math.floor(Math.random() * 50) + 50;
-  obstacle.style.backgroundColor = `rgb(${Math.floor(
-    Math.random() * 255
-  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-
+  obstacle.style.backgroundImage = `url(${
+    arrayObstacles[Math.floor(Math.random() * 11)]
+  })`;
+  obstacle.style.backgroundSize = "contain";
+  obstacle.style.backgroundRepeat = "no-repeat";
   const moveObstacle = () => {
     obstacleRight += 5;
     obstacle.style.right = obstacleRight + "px";
@@ -92,6 +129,12 @@ const control = (e) => {
 };
 
 const game = () => {
+  allScreen.style.backgroundImage = `url(${
+    arrayBackground[Math.floor(Math.random() * 9)]
+  })`;
+  character.style.backgroundImage = `url(${
+    arrayCharacters[Math.floor(Math.random() * 5)]
+  })`;
   alert("Welcome to the jump game!");
   setInterval(showScore, 100);
   generateObstacles();
